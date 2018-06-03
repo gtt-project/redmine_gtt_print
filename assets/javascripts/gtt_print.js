05752244aa0e16c4a04bdc7d9db9eaa80037d6f4
Daniel Kastl <daniel@georepublic.de>
2018-06-04 00:15:54 +0900

    Retreive print configurations

--- assets/javascripts/gtt_print.js
+++ assets/javascripts/gtt_print.js
@@ -1,4 +1,17 @@
-$(document).on('click', '.print_box input[type=button]', function(e){
-  console.log('TODO: submit print');
-  return false;
+$(function() {
+  var server = ($('form.print_box').attr('action')).replace(/\/?$/, '/');
+
+  if (server) {
+    $.getJSON(server + "print/apps.json", function (data) {
+      $.each(data, function(key, value) {
+        var option = $("<option></option>").attr("value",value).text(value);
+        $('form.print_box select').append(option);
+      });
+    });
+  }
+
+  $(document).on('click', '.print_box input[type=button]', function(e){
+    console.log('TODO: submit print');
+    return false;
+  });
 });
