3ea711ceffaeeb250b77d4655cf9ffe2a38da937
Jens Kraemer <jk@jkraemer.net>
2018-07-11 21:10:54 +0800

    settings UI for tracker<=>print configuration assignment

--- lib/redmine_gtt_print.rb
+++ lib/redmine_gtt_print.rb
@@ -8,8 +8,14 @@ module RedmineGttPrint
     Setting.plugin_redmine_gtt_print
   end
 
+  def self.tracker_config(tracker)
+    (settings['tracker_config'] || {})[tracker.id.to_s]
+  end
+
   def self.mapfish
     RequestStore.store[:mapfish] ||=
       RedmineGttPrint::Mapfish.new(host: settings['default_print_server'])
   end
+
+
 end
