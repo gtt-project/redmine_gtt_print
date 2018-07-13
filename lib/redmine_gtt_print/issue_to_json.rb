752e61d0180a22940095d52843aa1932efb471f7
Daniel Kastl <daniel@georepublic.de>
2018-07-13 15:18:23 +0900

    Makes attributes match (hard-coded) with expected variables

--- lib/redmine_gtt_print/issue_to_json.rb
+++ lib/redmine_gtt_print/issue_to_json.rb
@@ -31,8 +31,28 @@ module RedmineGttPrint
     def self.attributes_hash(issue)
       {
         # initially "title" was part of the print template, but it has been removed.
-        # we can configure other default values here:
-        # title: issue.subject,
+        id: issue.id,
+        subject: issue.subject,
+        description: issue.description,
+        project_id: "TBD",
+        project_name: "TBD",
+        tracker_id: "TBD",
+        tracker_name: "TBD",
+        status_id: "TBD",
+        status_name: "TBD",
+        priority_id: "TBD",
+        priority_name: "TBD",
+        author_id: "TBD",
+        author_name: "TBD",
+        assigned_to_id: "TBD",
+        assigned_to_name: "TBD",
+        is_private: issue.is_private,
+        start_date: issue.start_date,
+        done_date: "TBD",
+        estimated_hours: issue.estimated_hours,
+        total_estimated_hours: issue.total_estimated_hours,
+        created_on: issue.created_on,
+        updated_on: issue.updated_on,
       }
     end
 
@@ -76,7 +96,7 @@ module RedmineGttPrint
         ],
         scale: 25000,
         projection: "EPSG:3857",
-        dpi: 72
+        dpi: 300
       }
     end
 
