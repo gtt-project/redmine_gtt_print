bb80d7e67c0200d48312dbc0d5d883ab88ca4321
Jens Kraemer <jk@jkraemer.net>
2018-09-28 12:28:46 +0800

    fix integration test to work with current print.mycityreport.jp setup

--- test/integration/mapfish_test.rb
+++ test/integration/mapfish_test.rb
@@ -14,13 +14,13 @@ class MapfishTest < ActionDispatch::IntegrationTest
   setup do
     @issue = Issue.find 1
     @mapfish = RedmineGttPrint::Mapfish.new host: "https://print.mycityreport.jp"
-    Setting.plugin_redmine_gtt_print = {'tracker_config' => { @issue.tracker.id.to_s => 'DEMO_gtt' }}
+    Setting.plugin_redmine_gtt_print = {'tracker_config' => { @issue.tracker.id.to_s => 'default' }}
   end
 
   test "should have print configs" do
     assert configs = @mapfish.print_configs
     assert_equal Array, configs.class
-    assert configs.include?("DEMO_gtt")
+    assert configs.include?("default")
   end
 
   test "should handle nil print config" do
@@ -28,9 +28,9 @@ class MapfishTest < ActionDispatch::IntegrationTest
   end
 
   test "should have layouts for print config" do
-    assert layouts = @mapfish.layouts("DEMO_gtt")
+    assert layouts = @mapfish.layouts("default")
     assert_equal Array, layouts.class
-    assert layouts.include?("A4 portrait")
+    assert layouts.include?("default"), layouts.inspect
   end
 
   test "should get status" do
@@ -40,13 +40,19 @@ class MapfishTest < ActionDispatch::IntegrationTest
   test "should issue print job" do
     i = @issue
     i.update_attribute :geojson, test_geojson
-    job = GttPrintJob.new issue: i, layout: 'A4 portrait'
+    job = GttPrintJob.new issue: i, layout: 'default'
 
     assert r = @mapfish.print(job)
     assert r.success?
     assert ref = r.ref
-    sleep 3
-    assert_equal :done, @mapfish.get_status(ref)
+    status = nil
+    loop do
+      status = @mapfish.get_status(ref)
+      break if status == :done
+      assert_equal :running, status
+      sleep 2
+    end
+    assert_equal :done, status
     assert pdf = @mapfish.get_print(ref)
     (File.open('print.pdf', 'wb') << pdf).close
   end
