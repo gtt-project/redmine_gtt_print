0f3aa04df7d56484e452194dee193b248e52b137
Ko Nagase <nagase@georepublic.co.jp>
2021-02-15 18:24:09 +0900

    Adding server-example
    Adjusted local mapfish print server path
    
    Updated mapfish_print docker image version and adjusted font path
    
    Added server-example
    
    Signed-off-by: Ko Nagase <nagase@georepublic.co.jp>

--- init.rb
+++ init.rb
@@ -15,7 +15,7 @@ Redmine::Plugin.register :redmine_gtt_print do
 
   settings(
     default: {
-      'default_print_server' => "http://localhost:8080/mfp",
+      'default_print_server' => "http://localhost:8080",
       "tracker_config" => {},
       "issue_list_config" => nil,
       "default_print_server_timeout" => "5",
